5e6160bc2a31db67db37d7b838f40bb720f5cbff
Yun Huang <huangyunict@gmail.com>
2024-12-31 16:11:09 -0800

    add verify

--- markdown/problem_5.md
+++ markdown/problem_5.md
@@ -13,12 +13,9 @@
 ## 解法
 
 直接求这些整数的最小公倍数即可。
-算法部分的 Python 代码如下，完整的代码见 [solution_5.py](../solutions/solution_5.py)，这里直接调用 `sympy.lcm()` 求最小公倍数。
+算法部分的 Python 代码如下，完整的代码见 [solution_5.py](../solutions/solution_5.py)，这里直接调用 `math.lcm()` 求最小公倍数。
 
 ```python
-from sympy import lcm
-
-
 def solve_p5(n: int) -> int:
-    return lcm(range(1, n + 1))
+    return math.lcm(*list(range(1, n + 1)))
 ```
--- solutions/solution_5.py
+++ solutions/solution_5.py
@@ -1,10 +1,14 @@
-from sympy import lcm
+import math
+from assertpy import assert_that
 
 
 def solve_p5(n: int) -> int:
-    return lcm(range(1, n + 1))
+    return math.lcm(*list(range(1, n + 1)))
 
 
 if __name__ == '__main__':
-    print(solve_p5(10))  # 2520
+    # Verify.
+    assert_that(solve_p5(10)).is_equal_to(2520)
+    assert_that(solve_p5(20)).is_equal_to(232792560)
+    # Solution.
     print(solve_p5(20))  # 232792560
